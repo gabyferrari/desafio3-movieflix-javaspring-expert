d7747dd001582dd2cbeeac84d7f06fb611aadcf1
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-08-29 22:52:48 -0300

    Fixing findByGenre lazy query

--- src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
+++ src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
@@ -14,10 +14,14 @@ import com.devsuperior.movieflix.entities.Movie;
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 	
-	@Query("SELECT obj "
+	@Query(value = "SELECT obj "
 			+ "FROM Movie obj "
+			+ "JOIN FETCH obj.genre "
 			+ "WHERE (:genreId = 0 OR obj.genre.id = :genreId) "
-			+ "ORDER BY obj.title")
+			+ "ORDER BY obj.title",
+		   countQuery = "SELECT COUNT(obj) "
+					+ "FROM Movie obj "
+					+ "WHERE (:genreId = 0 OR obj.genre.id = :genreId) ")
 	Page<Movie> findByGenre(Long genreId, Pageable pageable);
 	
 	@Query("SELECT new com.devsuperior.movieflix.dto.ReviewDTO(obj.id, obj.text, obj.movie.id, obj.user.id, obj.user.name, obj.user.email) "
